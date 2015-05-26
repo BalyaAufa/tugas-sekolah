a3ac40733e51c9d28d9cb0c6c384b2a514383800
BalyaAufa <balyaaufa0@gmail.com>
2015-05-26 09:24:37 +0700

    Update balya_led.ino

--- balya_led.ino
+++ balya_led.ino
@@ -1,31 +1,31 @@
 void setup() {
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(2, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 
 }
 
 void loop() {
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
+  digitalWrite(2, HIGH);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
   delay(1000);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  delay(2000);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
+  digitalWrite(2, LOW);
   digitalWrite(10, HIGH);
   digitalWrite(11, LOW);
-  delay(3000);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
+  digitalWrite(12, LOW);
+  delay(2000);
+  digitalWrite(2, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay(3000);
+  digitalWrite(2, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
   delay(4000);
 
 }
